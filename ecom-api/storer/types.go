6498096c170e39b08c5f78fa8ead5776b0a5dbe0
Sxmmie <samuelumohjnr@gmail.com>
2026-09-18 19:18:10 +0100

    Added the Create product store

--- ecom-api/storer/types.go
+++ ecom-api/storer/types.go
@@ -3,7 +3,7 @@ package storer
 import "time"
 
 type Product struct {
-	ID           int64      `json:"id" db:"id"`
+	ID           int64      `json:"id" db:"id"` // json tag is used so that the req body can be marshalled into the struct, db tag is used to map each field to the field of the db queries
 	Name         string     `json:"name" db:"name"`
 	Image        string     `json:"image" db:"image"`
 	Category     string     `json:"category" db:"category"`
@@ -13,5 +13,5 @@ type Product struct {
 	Price        float64    `json:"price" db:"price"`
 	CountInStock int64      `json:"count_in_stock" db:"count_in_stock"`
 	CreatedAt    time.Time  `json:"created_at" db:"created_at"`
-	UpdatedAt    *time.Time `json:"updated_at" db:"updated_at"`
+	UpdatedAt    *time.Time `json:"updated_at" db:"updated_at"` // *pointer type becasue the field is nullable
 }
